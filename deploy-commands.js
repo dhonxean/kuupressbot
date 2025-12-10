6ab1ebbb926a508dc6f501987a7eec26de5618ed
xeankun <119888527+xeankun@users.noreply.github.com>
2025-12-10 21:36:54 +0800

    Deploying to zetro and kuupress

--- deploy-commands.js
+++ deploy-commands.js
@@ -31,13 +31,18 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN)
 async function deploy() {
     try {
         console.log('Refreshing application (/) commands...')
+        // await rest.put(
+        //     Routes.applicationGuildCommands(
+        //         process.env.DISCORD_CLIENT_ID, // your bot's application ID
+        //         process.env.DISCORD_GUILD_ID,  // the server ID
+        //     ),
+        //     { body: commands },
+        // )
         await rest.put(
-            Routes.applicationGuildCommands(
-                process.env.DISCORD_CLIENT_ID, // your bot's application ID
-                process.env.DISCORD_GUILD_ID,  // the server ID
-            ),
+            Routes.applicationCommands(process.env.CLIENT_ID),
             { body: commands },
         )
+
         console.log('✅ Successfully registered slash commands.')
     } catch (error) {
         console.error(error)
