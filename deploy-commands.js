ffa4ec04dc0a0297335b39f52cbea34addf4177b
xeankun <119888527+xeankun@users.noreply.github.com>
2025-12-10 21:29:46 +0800

    Updated to type module

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-if (!process.env.CLIENT_ID || !process.env.GUILD_ID || !process.env.DISCORD_TOKEN) {
+if (!process.env.DISCORD_CLIENT_ID || !process.env.DISCORD_GUILD_ID || !process.env.DISCORD_TOKEN) {
     console.error('❌ Missing CLIENT_ID, GUILD_ID or DISCORD_TOKEN in .env')
-    console.error('CLIENT_ID:', process.env.CLIENT_ID)
-    console.error('GUILD_ID:', process.env.GUILD_ID)
+    console.error('CLIENT_ID:', process.env.DISCORD_CLIENT_ID)
+    console.error('GUILD_ID:', process.env.DISCORD_GUILD_ID)
     process.exit(1)
 }
 
